326f3c6b1a477d3f788b9297ed43e99e93fa1281
Banana Viking <bananavikingcoder@gmail.com>
2019-02-15 12:56:35 -0600

    added startScanning() to VC

--- Project22/ViewController.swift
+++ Project22/ViewController.swift
@@ -25,9 +25,17 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         view.backgroundColor = UIColor.gray
     }
 
+    func startScanning() {
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.stopRangingBeacons(in: beaconRegion)
+    }
 
 }
 
+// MARK: - Location Manager Delegate Functions
 extension ViewController {
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedAlways {
